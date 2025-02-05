f394934853fb67aefc839f2472f9e8dd88a9cf12
Metriximor <metriximor@gmail.com>
2025-02-05 02:51:21 +0000

    Register Yamlify command

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/Introbook.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/Introbook.java
@@ -57,7 +57,7 @@ public class Introbook extends SimpleHack<IntrobookConfig> implements Listener, 
             plugin().log("Registering introbook command");
             plugin().registerCommand("introbook", this);
             plugin().registerCommand("giveintrobook", this);
-            plugin().registerCommand("spawnbook", this);
+            plugin().registerCommand("yamlify", this);
         }
     }
 
