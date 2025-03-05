7f009702227b303de5a3127e6acc35f773c23cbc
Metriximor <metriximor@gmail.com>
2025-03-05 01:46:35 +0000

    use exchangeutils to add items for me cause inventory handling is weird and complicated

--- plugins/civsim-paper/src/main/java/tutellemc/civsim/models/ShopOffer.java
+++ plugins/civsim-paper/src/main/java/tutellemc/civsim/models/ShopOffer.java
@@ -2,14 +2,10 @@ package tutellemc.civsim.models;
 
 import lombok.With;
 import org.bukkit.inventory.ItemStack;
-import tutellemc.civsim.CivSim;
 
 @With
 public record ShopOffer(ItemStack input, ItemStack output, int stock) {
     boolean isAffordableBy(final ItemStack wage) {
-        final boolean canAfford = input.getType().equals(wage.getType()) && wage.getAmount() >= input.getAmount();
-        CivSim.log()
-                .info("Analysing %s when compared to wage %s, can afford? %s".formatted(toString(), wage, canAfford));
-        return canAfford;
+        return input.getType().equals(wage.getType()) && wage.getAmount() >= input.getAmount();
     }
 }
--- plugins/civsim-paper/src/main/java/tutellemc/civsim/tasks/ProductionTask.java
+++ plugins/civsim-paper/src/main/java/tutellemc/civsim/tasks/ProductionTask.java
@@ -2,10 +2,12 @@ package tutellemc.civsim.tasks;
 
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitTask;
 import tutellemc.civsim.CivSim;
 import tutellemc.civsim.models.Mineshaft;
 import tutellemc.civsim.services.NodeService;
+import vg.civcraft.mc.civmodcore.inventory.InventoryUtils;
 
 @RequiredArgsConstructor
 public class ProductionTask implements Consumer<BukkitTask> {
@@ -20,6 +22,7 @@ public class ProductionTask implements Consumer<BukkitTask> {
     public void runTask() {
         CivSim.log().info("Running production task");
         nodeService.getNodes().stream().filter(Mineshaft::isActive).forEach(node -> node.getOutput()
-                .forEach(output -> node.getInventory().addItem(output)));
+                .forEach(output ->
+                        InventoryUtils.safelyAddItemsToInventory(node.getInventory(), new ItemStack[] {output})));
     }
 }
